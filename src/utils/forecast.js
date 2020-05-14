a73f078e0b96a689f46f87a74262bf40a2576d18
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-05-14 14:30:39 +0300

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('postman-request');
+
+const forecast = (latitude, longtitude, callback) => {
+
+  const url = 'http://api.weatherstack.com/forecast?access_key=7fddc13735898463f1e3b9c92fceaf0b&query=' + latitude + ',' + longtitude + '&units=f';
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to wather service!', undefined)
+    } else if (body.error) {
+      callback('Unable to find location!', undefined)
+    } else {
+      callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.');
+    }
+  });
+};
+
+module.exports = forecast;
