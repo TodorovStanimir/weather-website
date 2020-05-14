036b19f89b443b3a4611af525f1335ff9c28aea8
TodorovStanimir <52385215+TodorovStanimir@users.noreply.github.com>
2020-05-14 19:58:04 +0300

    Added additional information for temperature and hamidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('postman-request');
 
 const forecast = (latitude, longtitude, callback) => {
 
-  const url = 'http://api.weatherstack.com/forecast?access_key=7fddc13735898463f1e3b9c92fceaf0b&query=' + latitude + ',' + longtitude + '&units=f';
+  const url = 'http://api.weatherstack.com/forecast?access_key=7fddc13735898463f1e3b9c92fceaf0b&query=' + latitude + ',' + longtitude + '&units=m';
 
   request({ url, json: true }, (error, { body }) => {
     if (error) {
@@ -10,7 +10,8 @@ const forecast = (latitude, longtitude, callback) => {
     } else if (body.error) {
       callback('Unable to find location!', undefined)
     } else {
-      callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. There is a ' + body.current.precip + '% chance of rain.');
+      console.log(body);
+      callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out, feels like ' + body.current.feelslike + ' degrees. The hamidity is ' + body.current.humidity + '%. There is a ' + body.current.precip + '% chance of rain.');
     }
   });
 };
